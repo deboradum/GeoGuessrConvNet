0366e1fdbc36ad614635843c5b9b5aee2c527088
deboradum <pvwpvw@protonmail.com>
2024-12-16 19:32:29 +0100

    visualize torgether with other metrics gathering

--- visualize.py
+++ visualize.py
@@ -32,24 +32,10 @@ def create_prediction_figure(image, guess, ground_truth, output_path):
 
 
 # Dataloader should use a batch size of 60 (the number of test images per class)
-def visualize_predictions(net, dataloader, output_dir):
+def get_accuracies(net, dataloader, output_dir):
+    os.makedirs(os.path.dirname(output_dir), exist_ok=True)
+
     classes = dataloader.dataset.classes
-    for inputs, labels in dataloader:
-        logits = net(inputs)
-        predictions = torch.argmax(logits, dim=1).tolist()
-        ground_truth = labels.tolist()
-
-        inputs.tolist()
-
-        for i, (p, gt) in enumerate(zip(predictions, ground_truth)):
-            guess_class = classes[p]
-            gt_class = classes[gt]
-            img_number = i % 61  # 60 images per class. Assumes dataset is not shuffled.
-            output_path = f"{output_dir}/{gt_class}/{img_number}.png"
-            create_prediction_figure(inputs[i], guess_class, gt_class, output_path)
-
-
-def get_accuracies(net, dataloader, output_path):
     all_predictions = []
     all_ground_truth = []
     for inputs, labels in dataloader:
@@ -59,13 +45,22 @@ def get_accuracies(net, dataloader, output_path):
 
         all_predictions.extend(predictions)
         all_ground_truth.extend(ground_truth)
+
+        # Visualize prediction and ground truth
+        inputs.tolist()
+        for i, (p, gt) in enumerate(zip(predictions, ground_truth)):
+            guess_class = classes[p]
+            gt_class = classes[gt]
+            img_number = i % 61  # 60 images per class. Assumes dataset is not shuffled.
+            output_path = f"{output_dir}/visualizations/{gt_class}/{img_number}.png"
+            create_prediction_figure(inputs[i], guess_class, gt_class, output_path)
     classes = dataloader.dataset.classes
 
     report_dict = classification_report(
         all_ground_truth, all_predictions, target_names=classes, output_dict=True
     )
     report_df = pd.DataFrame(report_dict).transpose()
-    report_df.to_csv(output_path, index=True)
+    report_df.to_csv(f"{output_dir}/accuracies.csv", index=True)
 
     cm = confusion_matrix(all_ground_truth, all_predictions)
 
@@ -188,11 +183,8 @@ if __name__ == "__main__":
     net.load_state_dict(torch.load(pretrained_path, weights_only=True))
     net.eval()
 
-    # Visualize images and predictions
-    visualize_predictions(net, test_loader, f"{resnet}_results")
-
-    # Get accuracies in csv format
-    cm, classes = get_accuracies(net, test_loader, f"{resnet}_results.csv")
+    # Get accuracies in csv format and visualize predictions.
+    cm, classes = get_accuracies(net, test_loader, f"{resnet}_results")
 
     # Plot accuracies with tsne and umap
-    plot_tsne_umap(cm, classes, f"{resnet}_class_confusions")
+    plot_tsne_umap(cm, classes, f"{resnet}_results/{resnet}_class_confusions")
